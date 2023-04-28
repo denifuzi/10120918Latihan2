7170161ea672a1ca98db8f482087e83d532c4682
denifuzi <fuzideni@gmail.com>
2023-04-28 16:21:16 +0700

    initial commit

---
+++ app/src/main/java/com/akb/a10120918latihan2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.akb.a10120918latihan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
